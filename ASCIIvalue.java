76775f95120609692a021e14af03170faf50c63e
Sandeep Kumar <166711715+sandeepchiramdasu@users.noreply.github.com>
2024-09-14 11:47:33 +0530

    9

---
+++ ASCIIvalue.java
@@ -0,0 +1,9 @@
+// java program to find ascii value of a character
+public class ASCIIvalue {
+    public static void main(String[] args) {
+  
+        char c = 'a';
+        int assciValue = c;
+        System.out.println(assciValue);
+    }
+}
