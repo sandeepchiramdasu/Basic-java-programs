0ab3dc52cfc4aaf982358b2baaf9e7aaf93f2085
Sandeep Kumar <166711715+sandeepchiramdasu@users.noreply.github.com>
2024-09-18 11:04:44 +0530

    17

---
+++ FahrenheitToCelsius.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = sc.nextDouble();
+
+        double celsius = (fahrenheit - 32) * 5.0 / 9.0;
+
+        System.out.println("Temperature in Celsius: " + celsius);
+        sc.close();
+    }
+}
